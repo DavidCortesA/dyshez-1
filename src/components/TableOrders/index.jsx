f2e4a81b266ad4a842e1c831eb4bf69cc37a79c7
David N. Cortez Ayala <86697591+DavidCortesA@users.noreply.github.com>
2024-07-22 10:44:26 -0600

    Build page

--- src/components/TableOrders/index.jsx
+++ src/components/TableOrders/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { faker } from '@faker-js/faker';
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
